07a0289692a763c040148449a79bab4debc020bf
GenzaSN <stepan@geko-studio.com>
2021-08-27 09:51:57 +0700

    d

--- grafana_backup/save.py
+++ grafana_backup/save.py
@@ -3,7 +3,6 @@ from grafana_backup.save_dashboards import main as save_dashboards
 from grafana_backup.save_datasources import main as save_datasources
 from grafana_backup.save_folders import main as save_folders
 from grafana_backup.save_alert_channels import main as save_alert_channels
-from grafana_backup.archive import main as archive
 from grafana_backup.save_orgs import main as save_orgs
 from grafana_backup.save_users import main as save_users
 import sys
